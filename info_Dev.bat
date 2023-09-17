b6dd6cdbed14c9feaf72253bb74f402347fab4fe
Skiawm91 <96339124+Skiawm91@users.noreply.github.com>
2023-09-17 15:13:13 +0800

    Add files via upload

--- info_Dev.bat
+++ info_Dev.bat
@@ -1,11 +1,12 @@
-echo 4.00 (2a1556084dev)
+echo               Dev更新 4.00 (2a1556085dev)
 echo. 
-echo 1.將啟用功能整合至Settings
+echo 　　　　　更新內容　　　　　　　　　　Bug修復
+echo =========================== ==========================
+echo 1.將啟用功能整合至Settings  1.修復了自動更新窗口未隱藏
 echo 2.將關於整合至Settings
 echo 3.新的OOBE介面
 echo 4.開機動畫改變
 echo 5.將廢除的BSOD重新使用
 echo 6.自動更新
-echo 7.修復Bug
 echo.
-echo 建議所有Dev使用者更新
+echo                 (建議所有Dev使用者安裝)
